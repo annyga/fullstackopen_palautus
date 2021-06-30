66ac16d96069ad74a9c95090038a63676133c009
Andreas Nygård <andreas-nygard@hotmail.com>
2021-06-30 14:20:59 +0300

    tehtava 3.22

--- osa3/puhelinluettelon backend/index.js
+++ osa3/puhelinluettelon backend/index.js
@@ -2,6 +2,9 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+require('dotenv').config()
+const Person = require('./models/person');
+const { response } = require('express');
 
 const app = express();
 app.use(cors());
@@ -23,113 +26,100 @@ app.use(morgan(":method :url :status :res[content-length] - :response-time ms :r
 app.use(express.json());
 
 
-morgan.token('type', function (req, res) { return req.headers['content-type'] })
-let persons = [
 
-    {
-        "id": 1,
-        "name": "Arto Hellas",
-        "number": "040-123456"
-        
-      },
-      {
-        "id": 2,
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-        
-      },
-      {
-        "id": 3,
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-        
-      },
-      {
-        "id": 4,
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-        
-      },
-      {
-        "id": 5,
-        "name": "wer",
-        "number": "868-9871117"
-      }
-]
+let personsList = []
 
 
-app.get("/api/persons", (req, res) => {
-    res.json(persons)
+app.get("/api/persons", (req, res, next) => {
+    Person.find({}).then( persons => {
+        personsList = [...persons]
+        res.json(persons);
+    })
+    .catch(error => next(error))
 })
 
-app.get("/api/persons/:id", (req, res) => {
+app.get("/api/persons/:id", (req, res, next) => {
     
-    let pers = persons.find( item => item.id === Number(req.params.id))
-
-    if(pers){
-        res.json(pers)
-    }else{
-        res.sendStatus(404).end();
-    }
+    Person.findById(req.params.id)
+    .then( pers => res.json(pers))
+    .catch(error => next(error))
 })
 
 app.get("/info", (req, res) => {
-    let date = new Date()
-    let msg = `<p>Phonebook has info for ${persons.length} people</p><br><p>${date}<p/>`
-    
-    
-    res.send(msg)
-})
 
-app.post("/api/persons", (req, res) => {
-    let id = (Math.random() * 1000).toFixed(0);
-
-    let foundPersons = persons.filter( item => item.name === req.body.name)
-    
-    if(!req.body.name || !req.body.number){
-        return res.status(400).json({error: "content missing"})
-     
-    }else if(foundPersons.length > 0){    
-        return res.status(400).json({error: "Person allready exists"})
-    }else {
-        let newPers = {
-            id: Number(id),
-            name: req.body.name,
-            number: req.body.number
-        }
+    Person.find({}).then( persons => {
+        personsList = [...persons]
+        let date = new Date()
+        let msg = `<p>Phonebook has info for ${personsList.length} people</p><br><p>${date}<p/>`
         
-        persons.push(newPers)
-        res.send(newPers)
+        
+        res.send(msg)
+        
+    })
+    .catch(error => next(error))
+
+
+})
+
+app.post("/api/persons", (req, res, next) => {
+
+    const body = req.body;
+
+    if(body.name === undefined){
+        return res.status(400).json({error: "name is missing"})
     }
 
+    const person = new Person({
+        name: body.name,
+        number: body.number
+    })
+
+    person.save().then( savedPerson => {
+        res.json(savedPerson)
+    })
+    .catch(error => next(error))
 
 })
 
 
-app.delete("/api/persons/:id", (req, res) => {
-    persons = persons.filter( item => item.id !== Number(req.params.id))
+app.delete("/api/persons/:id", (req, res, next) => {
+    Person.findByIdAndRemove(req.params.id)
+    .then(result => {
+        res.status(204).end()
+    })
+    .catch(error => next(error))
 
-    res.sendStatus(204).end();
 })
 
-app.put("/api/persons/:id" , (req, res) => {
-    let id = Number(req.params.id);
+app.put("/api/persons/:id" , (req, res, next) => {
+
     let newMan = {
-        "id": id,
-        "name" :req.body.name,
-        "number" : req.body.number
+        name: req.body.name,
+        number: req.body.number
     }
-    console.log(newMan);
-    persons = persons.filter( item => item.id !== id)
-    persons.push(newMan)
-    
-    res.sendStatus(204).end();
+
+    Person.findByIdAndUpdate(req.params.id, newMan, {new: true})
+    .then(updatedPerson => res.json(updatedPerson))
+    .catch( error => next(error))
 
     
 })
 
 
-const PORT = 3001;
+const errorHandler = (error, req, res, next) => {
+
+    if(error.name === "CastError"){
+        return res.status(400).send({error: "Vääränlainen id"})
+    }else if (error.name === "ValidationError"){
+        return res.status(400).send({error: error.message})
+    }
+
+    next(error);
+}
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
---
+++ osa3/puhelinluettelon backend/models/person.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+var uniqueValidator = require('mongoose-unique-validator')
+
+const url = process.env.MONGODB_URI;
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+.then ( result => {
+    console.log("Connected to MongoDB Atlas")
+}).catch(error => {
+    console.log("error connecting")
+})
+
+const personSchema = new mongoose.Schema({
+    name: {type:String,
+            minlength: 3,
+            required: true,
+            unique: true},
+     number: {type: String,
+            minlength: 8,
+            required: true}
+    });
+
+personSchema.plugin(uniqueValidator);
+
+personSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model('Person', personSchema);
